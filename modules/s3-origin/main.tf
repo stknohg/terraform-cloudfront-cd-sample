8183deea2e543fc353e41453ea62bd505c1ac210
stknohg <stknohg@gmail.com>
2024-02-15 12:39:13 +0900

    Added sample index.html.

--- modules/s3-origin/main.tf
+++ modules/s3-origin/main.tf
@@ -12,3 +12,17 @@ resource "aws_s3_bucket_public_access_block" "this" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+// Sample index.html
+resource "aws_s3_object" "v1_index_html" {
+  bucket = aws_s3_bucket.this.id
+  key    = "/v1/index.html"
+  source = "${path.module}/v1-index.html"
+  content_type = "text/html"
+}
+resource "aws_s3_object" "v2_index_html" {
+  bucket = aws_s3_bucket.this.id
+  key    = "/v2/index.html"
+  source = "${path.module}/v2-index.html"
+  content_type = "text/html"
+}
